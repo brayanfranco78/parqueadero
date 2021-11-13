637ba5364278a1cc0165a864eea17195e70dc7cf
JuanDa237 <juandavid200301@hotmail.com>
2021-11-13 17:01:09 -0500

    - Create project - Create client handlers - Create mongodb connection

---
+++ src/datasources/mongo.datasource.ts
@@ -0,0 +1,24 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongo',
+  connector: 'mongodb',
+  url: 'mongodb+srv://user:user123@cluster0.pvgum.mongodb.net/parqueadero?retryWrites=true&w=majority',
+};
+
+@lifeCycleObserver('datasource')
+export class MongoDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
+  static dataSourceName = 'mongo';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongo', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
---
+++ src/models/cliente.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+  })
+  direccion?: string;
+
+  @property({
+    type: 'string',
+  })
+  telefono?: string;
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
